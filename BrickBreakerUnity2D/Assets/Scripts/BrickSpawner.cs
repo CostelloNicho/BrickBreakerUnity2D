fa31e238975c022a26d0836fa53fd17015e208a9
Nicholas Costello <nicholasjcostello@gmail.com>
2014-11-16 23:48:09 -0500

    Updated Brick prefab and brick spawner. Added a material and prefab for the ball.

--- BrickBreakerUnity2D/Assets/Scripts/BrickSpawner.cs
+++ BrickBreakerUnity2D/Assets/Scripts/BrickSpawner.cs
@@ -6,12 +6,13 @@ using UnityEngine;
 
 public class BrickSpawner : MonoBehaviour
 {
+    public GameObject Brick;
+    public Transform BrickContrainer;
     public List<GameObject> Bricks;
     public Color[] Colors;
 
     public float EdgeBufferZone = 1f;
     public float SpawnOffset = 0.5f;
-    public GameObject Brick;
 
     protected void Start()
     {
@@ -21,15 +22,14 @@ public class BrickSpawner : MonoBehaviour
 
     protected void CreateBrickBlock(int rows, int columns)
     {
-
         for (var column = 0; column < columns; column++)
         {
             for (var row = 0; row < rows; row++)
             {
                 var xMax = ResolutionManager.HalfWidth - EdgeBufferZone;
                 var yMax = ResolutionManager.HalfHeight - EdgeBufferZone;
-                var x = ((row + 1f)/rows)*(xMax*2f);
-                var y = ((column + 1f)/columns)*yMax;
+                var x = ( ( row + 1f ) / rows ) * ( xMax * 2f );
+                var y = ( ( column + 1f ) / columns ) * yMax;
                 var brickColor = Colors[Random.Range(0, Colors.Length)];
                 var brickPosition = new Vector2(x - ResolutionManager.HalfWidth, y);
                 SpawnBrick(brickPosition.ToVector3(), brickColor);
@@ -41,6 +41,8 @@ public class BrickSpawner : MonoBehaviour
     protected void SpawnBrick(Vector3 position, Color color)
     {
         var go = Instantiate(Brick, position, Quaternion.identity) as GameObject;
+        if (go == null) return;
+        go.transform.parent = BrickContrainer;
         go.GetComponent<SpriteRenderer>().color = color;
         Bricks.Add(go);
     }
