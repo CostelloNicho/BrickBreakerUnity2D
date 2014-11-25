aa7d861fcbd18ddb2ccfb02f9ee4a7d5bb3b4acc
Nicholas Costello <nicholasjcostello@gmail.com>
2014-11-25 01:52:04 -0500

    Added a scene for HUD testing

---
+++ BrickBreakerUnity2D/Assets/Scripts/Components/Brick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Brick : MonoBehaviour 
+{
+
+    public void OnCollisionEnter2D (Collision2D coll)
+    {
+        Debug.Log(coll.gameObject.tag);
+    }
+}
---
+++ BrickBreakerUnity2D/Assets/Scripts/Manager/HUDManager.cs
@@ -0,0 +1,36 @@
+﻿// Copyright 2014 Nicholas Costello <NicholasJCostello@gmail.com>
+
+using UnityEngine.UI;
+
+public class HUDManager : Singleton<HUDManager>
+{
+    public Text ScoreText;
+
+    private int _score;
+
+    public void Start()
+    {
+        _score = 0;
+        DisplayScore();
+    }
+
+    public void OnEnable ()
+    {
+        Messenger<int>.AddListener(BrickBreakerEvents.PlayerScore, OnPlayerScored);
+    }
+    public void OnDisable ()
+    {
+        Messenger<int>.RemoveListener(BrickBreakerEvents.PlayerScore, OnPlayerScored);
+    }
+
+    public void OnPlayerScored(int points)
+    {
+        _score += points;
+        DisplayScore();
+    }
+
+    private void DisplayScore()
+    {
+        ScoreText.text = string.Format("Score: {0}", _score);
+    }
+}
