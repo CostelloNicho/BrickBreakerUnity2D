f56ea7743dc8f654a5c3413f413d7ab4ebbb9f3c
Nicholas Costello <nicholasjcostello@gmail.com>
2014-11-25 02:52:21 -0500

    Added HUD to the main scene and made some material edits.

--- BrickBreakerUnity2D/Assets/Scripts/Components/Ball.cs
+++ BrickBreakerUnity2D/Assets/Scripts/Components/Ball.cs
@@ -1,13 +1,15 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿// Copyright 2014 Nicholas Costello <NicholasJCostello@gmail.com>
 
-public class Ball : MonoBehaviour {
+using UnityEngine;
 
-
-
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		Destroy(collision.gameObject);
-	}
-
+public class Ball : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Brick") return;
+        
+        //rigidbody2D.AddExplosionForce(100, collision.contacts[0].point.ToVector3(), 0.1f);
+        Destroy(collision.gameObject);
+        Messenger<int>.Broadcast(BrickBreakerEvents.PlayerScore, 10);
+    }
 }
